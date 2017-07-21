76052f31fc612a91e08084c9642ffad67f1a5ae2
seunadex <seun.adekunle@andela.com>
2017-07-21 22:23:39 +0100

    chore(npm): install node packages for server 149317299
    chore(server): setup express server 149317299

---
+++ server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+
+// Set up the express app
+const app = express();
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.get('*', (req, res) => res.status(200).send(
+'<center><h1>Document Management System</h1></center>'
+));
+
+module.exports = app;
