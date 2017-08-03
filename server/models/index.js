4e66bf1ae3842361285e8a2960bc7c0aa57c50ae
seunadex <seun.adekunle@andela.com>
2017-08-03 12:30:02 +0100

    chore(test) modify index.js

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,9 @@
 import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
@@ -11,9 +14,13 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialet: 'postgres'
+  });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialet: 'postgres'
+  });
 }
 
 fs
